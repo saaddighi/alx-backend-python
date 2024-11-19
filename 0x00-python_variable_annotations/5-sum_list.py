222cf0023800e77a754bcc1164f789a06b62dee8
saaddighi <saaddighi20@gmail.com>
2024-11-19 13:25:54 +0000

    ok

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def sum_list(input_list: list(float))-> float:
+    """a function that returns their sum as a float"""
+    for i in input_list:
+        sum = 0
+        sum =+ i
+    return sum
