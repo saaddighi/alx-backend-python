0419720fd24b7beedd6e946a9250c5bc2fd2dddb
saaddighi <saaddighi20@gmail.com>
2024-11-19 13:09:40 +0000

    ok

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import math
+"""math module for the floor of n"""
+
+def floor(n: float):
+    """a function that returns the floor of the float."""
+    return math.floor(n)
