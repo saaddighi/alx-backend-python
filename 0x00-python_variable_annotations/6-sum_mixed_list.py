031aafbdb97b37ea0dbe4f2c2726ff6039b08e61
saaddighi <saaddighi20@gmail.com>
2024-11-20 11:05:35 +0000

    ok

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+    """a function that takes a list mxd_lst of integers 
+    and floats and returns their sum as a float."""
+    sum = 0
+    for i in mxd_lst:
+        sum = sum + i
+    return float(sum)
