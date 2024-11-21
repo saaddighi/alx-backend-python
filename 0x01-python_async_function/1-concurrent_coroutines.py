3bd1f5be2104241e067201c1d183a3b61dc87bfc
saaddighi <saaddighi20@gmail.com>
2024-11-21 11:28:32 +0000

    ok

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import asyncio
+import typing
+import bisect
+
+
+wait_random = __import__('0-basic_async_syntax.py').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    res = []
+    for i in range(0, n):
+        wait_random(max_delay)
+        res.append(max_delay)
+    sorted_list = []
+    for element in res:
+        bisect.insort(sorted_list, element)
+
+    return sorted_list
