cfc75c3e7b41384b3d291c1ac70ba51274c53303
saaddighi <saaddighi20@gmail.com>
2024-11-21 11:10:35 +0000

    ok

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import asyncio
+"""asyncio module to await the task"""
 import random
+"""random module to randomly generate the delay time"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
